9e0c835f2c1a9a8d49f997f22f2234f2e0fcde29
bebyx <bebyx@protonmail.com>
2020-06-14 18:10:13 +0300

    Add markdown rendering with Redcarpet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def markdown(text)
+    raw Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+      :autolink => true, :space_after_headers => true,
+      :escape_html => true).render(text)
+  end
 end
