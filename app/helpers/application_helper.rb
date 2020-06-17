ef8b662c6232e73058237f50cf0839a3e9b3dc17
bebyx <bebyx@protonmail.com>
2020-06-18 01:16:39 +0300

    Redcarpet tweaks, impletement imageboard-like md

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,20 @@
 module ApplicationHelper
   def markdown(text)
-    raw Redcarpet::Markdown.new(Redcarpet::Render::HTML,
-      :autolink => true, :space_after_headers => true,
-      :escape_html => true).render(text)
+    raw Redcarpet::Markdown.new(BbsRender.new(
+      filter_html: true, no_links: true, no_images: true,
+      no_styles: true),
+      autolink: false, space_after_headers: true,
+      escape_html: true, strikethrough: true,
+      underline: true, superscript: true,
+      no_intra_emphasis: true, tables: false,
+      quote: false, disable_indented_code_blocks: true
+    ).render(text)
+  end
+end
+
+
+class BbsRender < Redcarpet::Render::HTML
+  def block_quote(quote)
+    %(<blockquote style="color: green;margin-left:0;">#{quote.insert(3, ">")}</blockquote>)
   end
 end
