03f38cd4d05aae861be0ed9d12b686c0d0e70256
bebyx <bebyx@protonmail.com>
2020-06-18 10:44:56 +0300

    Disable headers in markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,14 @@
 module ApplicationHelper
   def markdown(text)
     raw Redcarpet::Markdown.new(BbsRender.new(
-      filter_html: true, no_links: true, no_images: true,
-      no_styles: true),
+      no_links: true, no_images: true,
+      no_styles: true, escape_html: true,
+      prettify: true),
       autolink: false, space_after_headers: true,
-      escape_html: true, strikethrough: true,
-      underline: true, superscript: true,
-      no_intra_emphasis: true, tables: false,
-      quote: false, disable_indented_code_blocks: true
+      strikethrough: true, underline: true,
+      superscript: true, no_intra_emphasis: true,
+      tables: false, quote: false,
+      disable_indented_code_blocks: true
     ).render(text)
   end
 end
@@ -16,5 +17,10 @@ end
 class BbsRender < Redcarpet::Render::HTML
   def block_quote(quote)
     %(<blockquote style="color: green;margin-left:0;">#{quote.insert(3, ">")}</blockquote>)
+  end
+
+  def header(text, level)
+    level = "#"
+    "#{level + " " + text}"
   end
 end
